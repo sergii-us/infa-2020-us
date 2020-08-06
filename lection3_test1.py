3d0067560877fbabb2e2528d864d1b9e54f5effb
Sergii Us <us.sergii@gmail.com>
2020-08-05 20:53:10 -0600

    added yellow smile with red eyes

---
+++ lection3_test1.py
@@ -0,0 +1,15 @@
+from graph import *
+
+penColor(0, 0, 0)
+penSize(1)
+brushColor("yellow")
+circle(300, 250, 100)
+brushColor("red")
+circle(255, 230, 25)
+circle(350, 230, 20)
+brushColor("black")
+circle(255, 230, 12)
+circle(350, 230, 10)
+
+run()
+
